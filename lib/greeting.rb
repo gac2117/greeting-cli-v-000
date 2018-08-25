be58d9b7bca764e7a4faaf3f6b686a4bee58ec22
Grace Choi <oiograceoio@gmail.com>
2018-08-25 05:16:47 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,3 +1,5 @@
 def greeting(name)
   puts "Hi! I'm HAL! What's your name?" 
-  puts "Hello #{name}. It's nice to meet you" # code the #greeting method here!
+  puts "Hello #{name}. It's nice to meet you" 
+  end 
+  # code the #greeting method here!
